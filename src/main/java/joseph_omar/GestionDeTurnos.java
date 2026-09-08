2d67a3f6c55d58f29ac370bfd641f6a47422393a
JosepeXD <20250623@ricaldone.eud.sv>
2026-09-08 09:45:36 -0600

    Avances

--- src/main/java/joseph_omar/GestionDeTurnos.java
+++ src/main/java/joseph_omar/GestionDeTurnos.java
@@ -1,15 +1,26 @@
 package joseph_omar;
 
+import java.util.LinkedList;
+
 public class GestionDeTurnos {
+    private LinkedList<String> cola = new LinkedList<>();
     public Void agregarClienteNormal(String nombre){
+        cola.addLast(nombre);
+        System.out.println("Cliente normal: " +nombre + "Agregado");
         return null;
     }
 
     public Void agregarClienteVIP(String nombre){
+        cola.addFirst(nombre);
+        System.out.println("Cliente VIP: " +nombre + "Agregado");
         return null;
     }
 
     public Void atenderSiguienteCliente(){
+        if (!cola.isEmpty()){
+            String atendida = cola.pollFirst();
+            System.out.println();
+        }
         return null;
     }
 }
