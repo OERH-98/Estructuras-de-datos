d7bf9ce62d66f1a58046d62eab9ac0bd82232288
JosepeXD <20250623@ricaldone.eud.sv>
2026-09-08 09:28:57 -0600

    Metodos y clase creada

---
+++ src/main/java/joseph_omar/GestionDeTurnos.java
@@ -0,0 +1,15 @@
+package joseph_omar;
+
+public class GestionDeTurnos {
+    public Void agregarClienteNormal(String nombre){
+        return null;
+    }
+
+    public Void agregarClienteVIP(String nombre){
+        return null;
+    }
+
+    public Void atenderSiguienteCliente(){
+        return null;
+    }
+}
