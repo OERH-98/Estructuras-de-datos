0304136c75ff827195a1963eb08974d86e8be965
Omar <20240600@ricalodne.edu.sv>
2026-09-08 10:52:35 -0600

    Semifuncionalidad

--- src/main/java/joseph_omar/GestionDeTurnos.java
+++ src/main/java/joseph_omar/GestionDeTurnos.java
@@ -4,10 +4,11 @@ import java.util.LinkedList;
 
 public class GestionDeTurnos {
     private LinkedList<String> cola = new LinkedList<>();
+
     public Void agregarClienteNormal(String nombre){
 
         cola.addLast(nombre);
-        System.out.println("Cliente normal: " +nombre + "Agregado");
+        System.out.println("Cliente normal: " +nombre + " Agregado");
 
         return null;
 
@@ -16,7 +17,7 @@ public class GestionDeTurnos {
     public Void agregarClienteVIP(String nombre){
 
         cola.addFirst(nombre);
-        System.out.println("Cliente VIP: " +nombre + "Agregado");
+        System.out.println("Cliente VIP: " +nombre + " Agregado");
 
         return null;
 
@@ -24,11 +25,15 @@ public class GestionDeTurnos {
 
     public Void atenderSiguienteCliente(){
 
-        if (!cola.isEmpty()){
-            String atendida = cola.pollFirst();
-            System.out.println();
-        }
-        return null;
 
+        if (!cola.isEmpty()){
+            String atendido = cola.pollFirst();
+            System.out.println("Atendiendo a: " + atendido);
+
+            System.out.println("Lista de clientes pendientes por atender: " + cola);
+        }else{
+            System.out.println("No hay clientes en la cola");
+        }
+    return null;
     }
 }
--- src/main/java/joseph_omar/Main.java
+++ src/main/java/joseph_omar/Main.java
@@ -5,28 +5,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner escaneador = new Scanner(System.in);
+
+        Scanner nombre = new Scanner(System.in);
 
         Integer opcion = 0;
 
+        GestionDeTurnos gestionDeTurnos = new GestionDeTurnos();
+
         do{
-            System.out.println("Bienvenido al Menú de Acciones, ¿que desea hacer?");
+            System.out.println("\nBienvenido al Menú de Acciones, ¿que desea hacer?");
             System.out.println("\nElija las Siguientes Opciones:");
-            System.out.println("1-Gestión de Turnos bancarios");
-            System.out.println("2-Salir");
+            System.out.println("1-Agregar Cliente normal");
+            System.out.println("2-Agregar Cliente VIP");
+            System.out.println("3-Atender al siguiente cliente");
+            System.out.println("4-Salir de la interfaz");
 
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Bienvenido a la gestión de turno, se le gestionara el turno");
-                        break;
-                    case 2:
-                        System.out.println("Saliendo del programa, que tenga un lindo día mi estimado");
-                        break;
-                    default:
-                        System.out.println("Opción no valida intente de nuevo");
-                        break;
-                }
-        } while (opcion !=2);
+            Scanner escaneador = new Scanner(System.in);
 
+            try {
+                opcion = Integer.parseInt(String.valueOf(escaneador.nextInt()));
+            } catch (Exception e) {
+                System.out.println("Error, Escribe una opción valida");
+            }
+            String cliente;
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Bienvenido al apartado de agregar clientes normales, por favor, escriba su nombre");
+                    gestionDeTurnos.agregarClienteNormal(cliente = nombre.nextLine());
+                    break;
+                case 2:
+                    System.out.println("Bienvenido al apartado de agregar clientes VIP, por favor, escriba su nombre");
+                    gestionDeTurnos.agregarClienteVIP(cliente = nombre.nextLine());
+                    break;
+                case 3:
+                    System.out.println("Atender al siguiente cliente");
+                    gestionDeTurnos.atenderSiguienteCliente();
+                    break;
+                case 4:
+                    System.out.println("Saliendo de la interfaz, que tenga un lindo día");
+                    break;
+                default:
+                    System.out.println("Opción no valida intente de nuevo");
+                    break;
+            }
+        } while (opcion !=4);
+        System.out.println("ERROR");
     }
 }
